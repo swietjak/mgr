62d7cc9f047690adefccb2e3c71a7af011903ae3
swietjak <you@example.com>
2024-03-14 00:07:27 +0100

    feat: added sample mil solutions

---
+++ AttentionDeepMIL/model.py
@@ -0,0 +1,144 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Attention(nn.Module):
+    def __init__(self):
+        super(Attention, self).__init__()
+        self.M = 500
+        self.L = 128
+        self.ATTENTION_BRANCHES = 1
+
+        self.feature_extractor_part1 = nn.Sequential(
+            nn.Conv2d(1, 20, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(20, 50, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2)
+        )
+
+        self.feature_extractor_part2 = nn.Sequential(
+            nn.Linear(50 * 4 * 4, self.M),
+            nn.ReLU(),
+        )
+
+        self.attention = nn.Sequential(
+            nn.Linear(self.M, self.L), # matrix V
+            nn.Tanh(),
+            nn.Linear(self.L, self.ATTENTION_BRANCHES) # matrix w (or vector w if self.ATTENTION_BRANCHES==1)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(self.M*self.ATTENTION_BRANCHES, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = x.squeeze(0)
+
+        H = self.feature_extractor_part1(x)
+        H = H.view(-1, 50 * 4 * 4)
+        H = self.feature_extractor_part2(H)  # KxM
+
+        A = self.attention(H)  # KxATTENTION_BRANCHES
+        A = torch.transpose(A, 1, 0)  # ATTENTION_BRANCHESxK
+        A = F.softmax(A, dim=1)  # softmax over K
+
+        Z = torch.mm(A, H)  # ATTENTION_BRANCHESxM
+
+        Y_prob = self.classifier(Z)
+        Y_hat = torch.ge(Y_prob, 0.5).float()
+
+        return Y_prob, Y_hat, A
+
+    # AUXILIARY METHODS
+    def calculate_classification_error(self, X, Y):
+        Y = Y.float()
+        _, Y_hat, _ = self.forward(X)
+        error = 1. - Y_hat.eq(Y).cpu().float().mean().data.item()
+
+        return error, Y_hat
+
+    def calculate_objective(self, X, Y):
+        Y = Y.float()
+        Y_prob, _, A = self.forward(X)
+        Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
+        neg_log_likelihood = -1. * (Y * torch.log(Y_prob) + (1. - Y) * torch.log(1. - Y_prob))  # negative log bernoulli
+
+        return neg_log_likelihood, A
+
+class GatedAttention(nn.Module):
+    def __init__(self):
+        super(GatedAttention, self).__init__()
+        self.M = 500
+        self.L = 128
+        self.ATTENTION_BRANCHES = 1
+
+        self.feature_extractor_part1 = nn.Sequential(
+            nn.Conv2d(1, 20, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(20, 50, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2)
+        )
+
+        self.feature_extractor_part2 = nn.Sequential(
+            nn.Linear(50 * 4 * 4, self.M),
+            nn.ReLU(),
+        )
+
+        self.attention_V = nn.Sequential(
+            nn.Linear(self.M, self.L), # matrix V
+            nn.Tanh()
+        )
+
+        self.attention_U = nn.Sequential(
+            nn.Linear(self.M, self.L), # matrix U
+            nn.Sigmoid()
+        )
+
+        self.attention_w = nn.Linear(self.L, self.ATTENTION_BRANCHES) # matrix w (or vector w if self.ATTENTION_BRANCHES==1)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(self.M*self.ATTENTION_BRANCHES, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = x.squeeze(0)
+
+        H = self.feature_extractor_part1(x)
+        H = H.view(-1, 50 * 4 * 4)
+        H = self.feature_extractor_part2(H)  # KxM
+
+        A_V = self.attention_V(H)  # KxL
+        A_U = self.attention_U(H)  # KxL
+        A = self.attention_w(A_V * A_U) # element wise multiplication # KxATTENTION_BRANCHES
+        A = torch.transpose(A, 1, 0)  # ATTENTION_BRANCHESxK
+        A = F.softmax(A, dim=1)  # softmax over K
+
+        Z = torch.mm(A, H)  # ATTENTION_BRANCHESxM
+
+        Y_prob = self.classifier(Z)
+        Y_hat = torch.ge(Y_prob, 0.5).float()
+
+        return Y_prob, Y_hat, A
+
+    # AUXILIARY METHODS
+    def calculate_classification_error(self, X, Y):
+        Y = Y.float()
+        _, Y_hat, _ = self.forward(X)
+        error = 1. - Y_hat.eq(Y).cpu().float().mean().item()
+
+        return error, Y_hat
+
+    def calculate_objective(self, X, Y):
+        Y = Y.float()
+        Y_prob, _, A = self.forward(X)
+        Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
+        neg_log_likelihood = -1. * (Y * torch.log(Y_prob) + (1. - Y) * torch.log(1. - Y_prob))  # negative log bernoulli
+
+        return neg_log_likelihood, A
